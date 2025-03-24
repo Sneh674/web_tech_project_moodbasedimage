7377913e48cd7d5579d83629732885924974eee6
Sneh674 <naiksneh6@gmail.com>
2025-03-24 09:38:48 +0530

    input for mood

--- mood-gallery-frontend/src/components/Home.jsx
+++ mood-gallery-frontend/src/components/Home.jsx
@@ -1,9 +1,32 @@
-const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+import "../styles/Home.css";
+import { useState } from "react";
 
-export default Home
+const Home = () => {
+    const [mood, setMood] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(mood);
+    };
+
+    return (
+        <div className="homeMain">
+            <div className="title">Moodbased Artgallery</div>
+            <div className="moodinput">
+                <form onSubmit={handleSubmit}>
+                    <select id="usermood" onChange={(e) => setMood(e.target.value)} defaultValue="">
+                        <option value="" disabled>Select your mood</option>
+                        <option value="happy">Happy</option>
+                        <option value="sad">Sad</option>
+                        <option value="angry">Angry</option>
+                        <option value="relaxed">Relaxed</option>
+                        <option value="excited">Excited</option>
+                    </select>
+                    <input type="submit" value="Enter" />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
